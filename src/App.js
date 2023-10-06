c5348bcb18960773442672973b3718f484d462b9
Hitesh <hiteshpatidarhp020>
2023-10-06 16:35:31 +0530

    test changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div >
-      App
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
