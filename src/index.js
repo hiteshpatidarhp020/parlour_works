c586206f39fad7a309425111c21ff237cb6fa3b6
Hitesh <hiteshpatidarhp020>
2023-10-15 11:59:32 +0530

    dropdown component

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import PwButton from "./lib/design-system/component/pw-button";
 import IcPlus from "./lib/design-system/icons/IcPlus";
-
+import ButtonDarkExample from "./lib/design-system/component/pw-dropdown";
+import "../node_modules/react-bootstrap/dist/react-bootstrap";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <div>
-      <PwButton kind="primary" prefix={<IcPlus />} suffix={<IcPlus />}>
+      <ButtonDarkExample width="100px" />
+      {/*<PwButton kind="primary" prefix={<IcPlus />} suffix={<IcPlus />}>
         New sale primary
       </PwButton>
       <PwButton
@@ -27,7 +30,7 @@ root.render(
       <PwButton kind="tertiary">Tertiary Button</PwButton>
       <PwButton kind="floating" prefix={<IcPlus />}>
         floating Button
-      </PwButton>
+</PwButton>*/}
     </div>
   </React.StrictMode>
 );
