8db7c553125e80a9c74781a45255df41cc247be1
Hitesh <hiteshpatidarhp020>
2023-12-06 17:15:57 +0530

    added new component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3c6c020c445b79b1"; }
+/******/ 		__webpack_require__.h = function() { return "67b0f97e820a2f83"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
