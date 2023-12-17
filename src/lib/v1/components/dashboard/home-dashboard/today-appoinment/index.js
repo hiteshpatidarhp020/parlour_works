a2a93aa5182ef96497c69d245c9986888b16977d
Monika Patidar <monikapatidar0907@gmail.com>
2023-12-17 13:37:48 +0530

    "src"

--- src/lib/v1/components/dashboard/home-dashboard/today-appoinment/index.js
+++ src/lib/v1/components/dashboard/home-dashboard/today-appoinment/index.js
@@ -1,8 +1,8 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import styles from "./todayapp.module.css";
-import AppoinmentTc from "../../../../../design-system/component/common/appoinment-totalcount";
-import MarkCompleted from "../../../../../design-system/component/common/mark-completed";
+import AppoinmentTc from "../../../../../design-system/component/common/today-appoinment/appoinment-totalcount";
+import MarkCompleted from "../../../../../design-system/component/common/today-appoinment/mark-completed";
 import PwLink from "../../../../../design-system/component/pw-link";
 
 const TodayAppoinment = ()=>{
